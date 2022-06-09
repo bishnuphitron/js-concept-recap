d43586b56750b3d9349dfabf296e5ece3097acf0
bsnun8 <bishnunath018@gmail.com>
2022-06-09 11:07:31 +0600

    home work

---
+++ practice.js
@@ -0,0 +1,16 @@
+// core concepts
+// variable (let, const) javascript var, let, const 
+// array, loop (while, for), conditionals, function
+
+// convert inch to feet
+// leap year
+// odd even
+// factorial (while, for)
+
+// New home work
+// 1. celcius to farenheit 
+// 2. farenheit to celcius
+// 3. grade calculation
+// 4. simple interest 
+
+
